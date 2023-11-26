5d56d6a8e8b5284c363452fcfccdba3bb2f40ce6
William-Ryan <billy.ryan.rom@gmail.com>
2023-11-25 23:56:23 -0500

    Add introduction, pictures, and statement to about page * Add Card contact to contact page

--- star/src/Components/Contact.jsx
+++ star/src/Components/Contact.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 
 import Typography from '@mui/material/Typography';
-import { makeStyles } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 
 const Contact = () => {
     return (
-        <div style={{ backgroundImage: "url(https://scontent-iad3-2.xx.fbcdn.net/v/t39.30808-6/308787210_457577689737886_6832976261704682608_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=5jo5PxPBv8MAX8ZJWsR&_nc_ht=scontent-iad3-2.xx&oh=00_AfA6j9bF3xhF0zyoI0z3-NfPjnw9MydGskVB2WMxnJ9fqw&oe=65671496)", backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center"}}>
-            <Typography variant={"h3"}>Contact Page</Typography>
-            <Typography>
-                <ul>
-                    <li>Address: 233 W Artisan St, Williamsport, MD</li>
-                    <li>Number: 123-123-1234</li>
-                    <li>Email: churchy@gmail.com</li>
-                </ul>
-            </Typography>
+        <div style={{ backgroundImage: "url(https://scontent-iad3-2.xx.fbcdn.net/v/t39.30808-6/308787210_457577689737886_6832976261704682608_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=5jo5PxPBv8MAX8ZJWsR&_nc_ht=scontent-iad3-2.xx&oh=00_AfA6j9bF3xhF0zyoI0z3-NfPjnw9MydGskVB2WMxnJ9fqw&oe=65671496)", backgroundRepeat: "no-repeat", backgroundSize: "22%", backgroundPosition: "center"}}>
+            <Card style={{ width: "23%", margin: "10% auto", fontSize: "1.5rem", textAlign: "center", opacity: "90%" }}>
+                <CardContent>
+                    <Typography>
+                        Contact Us
+                    </Typography>
+                    <ul>
+                        <li>Address: 233 W Artisan St, Williamsport, MD </li>
+                        <li>Number: 240-520-7202 </li>
+                        <li>Email: church@gmail.com </li>
+                    </ul>
+                </CardContent>
+            </Card>
         </div>
     )
 }
