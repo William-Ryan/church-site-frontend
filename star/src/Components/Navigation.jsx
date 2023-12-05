e6d9d96e90fa6b415222d49d35737a9c961739a2
William-Ryan <billy.ryan.rom@gmail.com>
2023-12-05 12:20:14 -0500

    Add styled components for media query on navigation

--- star/src/Components/Navigation.jsx
+++ star/src/Components/Navigation.jsx
@@ -5,6 +5,31 @@ import AppBar from '@mui/material/AppBar';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography'
+import { styled } from '@mui/material/styles'
+
+const Container = styled('div')(({theme}) => ({
+  display: "flex",
+  padding: "0% 0% 0.5% 0%",
+  backgroundColor: "black",
+  [theme.breakpoints.down('lg')]: {
+    flexDirection: "column"
+  }
+}))
+
+const ImageContainer = styled('div')(({theme}) => ({
+  display: "flex",
+  justifyContent: "right",
+  [theme.breakpoints.down('lg')]: {
+    justifyContent: 'left'
+  }
+}))
+
+const ChurchImg = styled('img')(({theme}) => ({
+  width: "15%",
+  [theme.breakpoints.down('lg')]: {
+    width: "6%"
+  }
+}))
 
 export default function Navigation() {
   const [value, setValue] = React.useState(0);
@@ -14,7 +39,7 @@ export default function Navigation() {
   };
 
   return (
-    <div style={{ display: "flex", padding: "0% 0% 0.5% 0%", backgroundColor: "black"}}>
+    <Container>
       <div style={{ width: "27%"}}>
             <Typography variant={"h1"} style={{ color: "deepskyblue", fontSize: "2rem", margin: "3% 0% 0% 2%"}}>
                 Star Of Hope Chapel
@@ -40,14 +65,13 @@ export default function Navigation() {
         </Tabs>
       </AppBar>
       </div>
-      <div style={{ display: "flex", justifyContent: "right"}}>
-          <img 
+      <ImageContainer>
+          <ChurchImg 
           src="https://scontent-iad3-1.xx.fbcdn.net/v/t1.18169-9/10624727_710356892396045_1093676820065097135_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=2be8e3&_nc_ohc=C-jK4jwVivMAX-oC_aJ&_nc_ht=scontent-iad3-1.xx&oh=00_AfD_avYfjvBZ-ubP_kAVZGcypadFvq5uQOxFvFUkRQPrAg&oe=658321CC"
           alt="Star Of Hope Church when first built"
-          style={{ width: "15%"}}
           />
-      </div>
-    </div>
+      </ImageContainer>
+    </Container>
   );
 }
 
